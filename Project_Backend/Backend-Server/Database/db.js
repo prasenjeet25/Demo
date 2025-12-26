16cf05ef972b9228094f475b3a0b5a2165f00642
Rushikesh Shivanand Anantpure <rushikeksh4567@gmail.com>
2025-12-26 10:06:00 +0530

    Delete Project_Backend/Backend-Server directory

--- Project_Backend/Backend-Server/Database/db.js
+++
@@ -1,9 +0,0 @@
-const mysql2 = require("mysql2")
-const pool = mysql2.createPool({
-    host:"localhost",
-    user:"root",
-    password:"manager",
-    database:"learning_platform"
-})
-
-module.exports = pool
